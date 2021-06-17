09954da74ca34f1fe85d85efc2371fc82041dbbb
ptstp <animalfusion88@gmail.com>
2021-06-17 20:08:59 +0300

    post users in json

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,26 +1,35 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useCallback } from 'react'
 import { AuthContext } from '../context/AuthContext'
 //import './App.css'
 
 export const Login = () => {
-  const auth = useContext(AuthContext) 
+  const auth = useContext(AuthContext)
   const [login, setLogin] = useState<string>('')
   const [pass, setPass] = useState<string>('')
   const loginChangeHandler = (event: any) => {
-  setLogin(event.target.value)  
+    setLogin(event.target.value)
   }
   const passwordChangeHandler = (event: any) => {
-  setPass(event.target.value)
-  console.log(pass)
+    setPass(event.target.value)
+    console.log(pass)
   }
 
+  const sendUsers = useCallback(async (_login, _password) => {
+    const user = {
+      login: _login,
+      password: _password
+    }
+    const jsonUser = JSON.stringify(user)
+    const request = await fetch('http://185.206.212.246:1234/api/auth/auth/', {method: 'POST', body: jsonUser, headers: {"Accept": "application/json", "Content-Type" : "application/json"} })
+    const data = await request.json()
+  }, [])
   const clickHandler = () => {
-    if (login !== 'admin' || pass !== '12345' ){
+    sendUsers(login, pass)
+    if (login !== 'admin' || pass !== '12345') {
       alert('FALL!!FUCKYOU!TRYAGAIN!!')
       return
-
-    } 
-  auth.login(login, pass)
+    }
+    auth.login(login, pass)
   }
   if (auth.isAuth == true) {
     return (
@@ -32,9 +41,9 @@ export const Login = () => {
   return (
     <div className="container">
       <h1>Login!</h1>
-      <input type = "input" onChange = {loginChangeHandler} />
-      <input type = "input" onChange = {passwordChangeHandler} />
-      <button onClick = {clickHandler} >Click</button>
+      <input type="input" onChange={loginChangeHandler} />
+      <input type="input" onChange={passwordChangeHandler} />
+      <button onClick={clickHandler} >Click</button>
     </div>
   )
 }
